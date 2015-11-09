41cedd6612288f9e74778c5791c0ace29c025c3d
Marco Filetti <marco.filetti@hiit.fi>
2015-11-09 19:45:12 +0200

    log fix

--- JustUsed/Model/AppSingleton.swift
+++ JustUsed/Model/AppSingleton.swift
@@ -17,15 +17,12 @@ class AppSingleton {
     static let log = AppSingleton.createLog()
     
     static func createLog() -> XCGLogger {
-        var error: NSError? = nil
         var firstLine: String = "Log directory succesfully created / present"
         let tempURL = NSURL(fileURLWithPath: NSTemporaryDirectory()).URLByAppendingPathComponent(NSBundle.mainBundle().bundleIdentifier!)
-        let tempDirBase = tempURL.path!
         do {
-            try NSFileManager.defaultManager().createDirectoryAtPath(tempDirBase, withIntermediateDirectories: true, attributes: nil)
-        } catch let error1 as NSError {
-            error = error1
-            firstLine = "Error creating log directory: " + error!.description
+            try NSFileManager.defaultManager().createDirectoryAtURL(tempURL, withIntermediateDirectories: true, attributes: nil)
+        } catch {
+            firstLine = "Error creating log directory: \(error)"
         }
         let logFilePath = tempURL.URLByAppendingPathComponent("XCGLog.log")
         let newLog = XCGLogger.defaultInstance()
